3bc4dc52abe8bd378ea6d6b2be1cca9f551d2658
Avraham Shukron <avraham.shukron@gmail.com>
2016-05-27 01:16:49 +0300

    converted 4 spaces to tabs

--- debug.h
+++ debug.h
@@ -9,17 +9,17 @@
 
 static inline void vdebug(const char *format, va_list argp)
 {
-    vprintf(format, argp);
+	vprintf(format, argp);
 }
 
 static inline void debug(const char *format, ...)
 {
-    if (DEBUG) {
-        va_list argp;
-	    va_start(argp, format);
-        vdebug(format, argp);
-        va_end(argp);
-    }
+	if (DEBUG) {
+		va_list argp;
+		va_start(argp, format);
+		vdebug(format, argp);
+		va_end(argp);
+	}
 }
 
 #endif /* DEBUG_H */
